d6cb20e9b1f43a18f4e0824006e05c5b9901defb
Meowmaster <ashenoy64@gmail.com>
2025-03-23 18:53:40 +0530

    Formatted Rag

--- Rag/__init__.py
+++ Rag/__init__.py
@@ -1,7 +1,7 @@
 from .rag import conversation_chain
 
 
-def rag_chain(vector_store, config ):
+def rag_chain( vector_store, config ):
     return conversation_chain(
         vector_store,
         config.llm_model, 
--- Rag/rag.py
+++ Rag/rag.py
@@ -1,10 +1,10 @@
 from langchain.chains import ConversationalRetrievalChain
 
 
-def conversation_chain(vector_store, llm, memory):
+def conversation_chain( vector_store, llm, memory ):
     retriever = vector_store.as_retriever()
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm, 
+        llm=llm,
         retriever=retriever,
         memory=memory
     )
