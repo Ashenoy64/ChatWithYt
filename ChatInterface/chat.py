10a3517f00a03d3e75e0816040de7c4082a43661
Meowmaster <ashenoy64@gmail.com>
2025-03-23 15:22:19 +0530

    Added ChatInterface (Not Tested)

---
+++ ChatInterface/chat.py
@@ -0,0 +1,20 @@
+import gradio as gr
+import os
+
+def chat(question, history, conversation_chain):
+    result = conversation_chain.invoke({"question": question})
+    return result["answer"]
+
+def create_chat_interface(rag_chain):
+    def wrapper(question, history):
+        return chat(question, history, rag_chain)
+    
+    interface =  gr.ChatInterface(
+        wrapper,
+        title=f"ChatWithYT",
+        description="Ask questions about the youtube videos",
+        type="messages",
+    )
+    return interface
+
+
