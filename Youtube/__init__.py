d1200e83e32313380d3f52caf8d71e94a0284a11
Meowmaster <ashenoy64@gmail.com>
2025-03-23 19:01:13 +0530

    Formatted Youtube

--- Youtube/__init__.py
+++ Youtube/__init__.py
@@ -1,14 +1,13 @@
 from .download import __download_audio
-import os
 
-def download_audios(urls, config):
+def download_audios( urls, config ):
     audio_files = []
     for url in urls:
-        _file = __download_audio(url, output_path = config.temp_dir)
+        _file = __download_audio( url, output_path = config.temp_dir )
         if _file:
-            audio_files.append(_file)
+            audio_files.append( _file )
     return audio_files
 
 
-def download_audio(url, config):
-    return __download_audio(url, config.temp_dir)
+def download_audio( url, config ):
+    return __download_audio( url, config.temp_dir )
--- Youtube/download.py
+++ Youtube/download.py
@@ -3,30 +3,30 @@ from pydub import AudioSegment
 import os
 
 
-def __download_audio(youtube_url, output_path="."):
+def __download_audio( youtube_url, output_path="." ):
     try:
-        yt = Yt(youtube_url)
-        audio_stream = yt.streams.filter(only_audio=True).first()
+        yt = Yt( youtube_url )
+        audio_stream = yt.streams.filter( only_audio=True ).first()
         if audio_stream:
-            print(f"Downloading audio from: {yt.title}")
-            downloaded_file = audio_stream.download(output_path)
-            print(f"Audio downloaded successfully to: {downloaded_file}")
-            _, file_ext = os.path.splitext(downloaded_file)
-            input_format = file_ext[1:] if file_ext.startswith('.') else file_ext
-            audio = AudioSegment.from_file(downloaded_file, format=input_format)
-            outName = yt.title.replace(" ", "_")
+            print( f"Downloading audio from: {yt.title}" )
+            downloaded_file = audio_stream.download( output_path )
+            print( f"Audio downloaded successfully to: {downloaded_file}" )
+            _, file_ext = os.path.splitext( downloaded_file )
+            input_format = file_ext[ 1: ] if file_ext.startswith( '.' ) else file_ext
+            audio = AudioSegment.from_file( downloaded_file, format=input_format )
+            outName = yt.title.replace( " ", "_" )
             mp3_filename = outName + ".mp3"
-            audio.export(os.path.join(output_path, mp3_filename), format="mp3")
-            print(f"Audio converted to MP3 and saved as: {mp3_filename}")
-            os.remove(downloaded_file)
+            audio.export( os.path.join( output_path, mp3_filename ), format="mp3" )
+            print( f"Audio converted to MP3 and saved as: {mp3_filename}" )
+            os.remove( downloaded_file )
             return mp3_filename
         else:
-            print("No audio stream found.")
+            print( "No audio stream found." )
             return None
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print( f"An error occurred: {e}" )
         return None
 
 
 if __name__ == "__main__":
-    __download_audio("https://www.youtube.com/watch?v=5qap5aO4i9A")
+    __download_audio( "https://www.youtube.com/watch?v=5qap5aO4i9A" )
