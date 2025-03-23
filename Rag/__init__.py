ff5b7c520e353128734dc6cbe753a07e10ffe424
Meowmaster <ashenoy64@gmail.com>
2025-03-23 15:22:38 +0530

    Added Rag Chain

---
+++ Rag/__init__.py
@@ -0,0 +1,9 @@
+from .rag import conversation_chain
+
+
+def rag_chain(vector_store, config ):
+    return conversation_chain(
+        vector_store,
+        config.llm_model, 
+        config.chat_memory
+    )
---
+++ Rag/rag.py
@@ -0,0 +1,11 @@
+from langchain.chains import ConversationalRetrievalChain
+
+
+def conversation_chain(vector_store, llm, memory):
+    retriever = vector_store.as_retriever()
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        retriever=retriever,
+        memory=memory
+    )
+    return conversation_chain
